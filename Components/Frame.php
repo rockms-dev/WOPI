89c037c50625dd831413557218ff0250fd2b73e7
Majid <majid.qureshi@cooperativecomputing.com>
2023-12-18 20:59:09 +0500

    update access token identity

--- Components/Frame.php
+++ Components/Frame.php
@@ -21,7 +21,7 @@ class Frame extends Component
     {
         $document = app(AbstractDocumentManager::class)::find($id);
 
-        $this->access_token = encrypt(Str::random(5).'_'.auth()->id());
+        $this->access_token = encrypt(Str::random(5));
         $this->ttl = strtotime("+10 hours")*1000;
         $this->url = $document->getUrlForAction('view', [
             'ui' => 'en',
